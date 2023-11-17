caddf8c2f275db428e76a88bf183b0b66dc79d65
justint <justintolbertpro@gmail.com>
2023-11-17 15:10:14 -0500

    Finished code.

--- src/main/java/edu/desu/exam02/part01/DLinkedList.java
+++ src/main/java/edu/desu/exam02/part01/DLinkedList.java
@@ -3,16 +3,16 @@ package edu.desu.exam02.part01;
 public class DLinkedList<T> {
     private Node<T> head;
 
-    public DLinkedList(){
+    public DLinkedList() {
     }
 
-    public T getData(){
-        if(head != null)
+    public T getData() {
+        if (head != null)
             return head.getData();
         return null;
     }
 
-    public void insertAtHead(T data){
+    public void insertAtHead(T data) {
         Node<T> node = new Node<>(data);
         if (head != null) {
             head.setPrev(node);
@@ -21,38 +21,69 @@ public class DLinkedList<T> {
         head = node;
     }
 
-    public void insertAtEnd(T data){
+    public void insertAtEnd(T data) {
         Node<T> nextNode = new Node<>(data);
-        if(head == null){
+        if (head == null) {
             head = nextNode;
-        }else {
+        } else {
             Node<T> current = head;
-            while(current.hasNext()){
+            while (current.hasNext()) {
                 current = current.getNext();
             }
             current.setNext(nextNode);
         }
     }
 
-    public Boolean contains(T data){
-        return null;
-    }
-
-    public Boolean containsDuplicates(){
-       return null;
-    }
-
-    public void removeDuplicates(){
-    }
-
-    public String toString(){
-        StringBuilder output = new StringBuilder();
-        Node<T> current = head;
-        while(current != null){
-            T data = current.getData();
-            output.append(data.toString()+ " ");
-            current = current.getNext();
+    public Boolean contains(T data) {
+        Node<T> node = this.head;
+        while (node != null) {
+            if (node.getData() == data) {
+                return true;
+            }
+            node = node.getNext();
         }
-        return output.toString().trim();
+        return false;
     }
-}
+
+    public Boolean containsDuplicates() {
+        Node<T> currentNode = this.head;
+        while (currentNode != null) {
+            Node checker = currentNode.getNext();
+            while (checker != null) {
+                if (currentNode.getData() == checker.getData()) {
+                    return true;
+                }
+                checker = checker.getNext();
+            }
+            currentNode = currentNode.getNext();
+        }
+        return false;
+    }
+
+    public void removeDuplicates() {
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            Node<T> checker = currentNode;
+            while (checker.getNext() != null) {
+                if (currentNode.getData().equals(checker.getNext().getData())) {
+                    checker.setNext(checker.getNext().getNext());
+                } else {
+                    checker = checker.getNext();
+                }
+            }
+            currentNode = currentNode.getNext();
+        }
+    }
+
+        public String toString() {
+            StringBuilder output = new StringBuilder();
+            Node<T> current = head;
+            while (current != null) {
+                T data = current.getData();
+                output.append(data.toString() + " ");
+                current = current.getNext();
+            }
+            return output.toString().trim();
+        }
+    }
+
--- src/main/java/edu/desu/exam02/part02/Recursion.java
+++ src/main/java/edu/desu/exam02/part02/Recursion.java
@@ -3,6 +3,13 @@ package edu.desu.exam02.part02;
 public class Recursion {
 
     public int countSubString(String pattern, String word){
-        return -1;
+        int answer = 0;
+        if (word.length() < pattern.length()){
+            return answer;
+        }
+        if (pattern.equals(word.substring(0,pattern.length()))){
+            answer++;
+        }
+        return answer + countSubString(pattern, word.substring(1));
     }
 }
